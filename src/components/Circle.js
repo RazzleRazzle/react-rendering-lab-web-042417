1d5dd2c90cc2212e868019a4496c1e08c088e851
Jake Musich <makejusich@gmail.com>
2017-07-18 10:49:05 -0400

    Done.

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 class Circle extends React.Component {
 
+  //returns a boolean value, if current props is not equal to new color
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps)
+    return(this.props.color !== nextProps.color)
+  }
+
   render() {
     const { color } = this.props;
     console.log(`A ${color} circle was rendered!`);
-    
+
     return (
       <div className={`circle ${color}`}></div>
     )
